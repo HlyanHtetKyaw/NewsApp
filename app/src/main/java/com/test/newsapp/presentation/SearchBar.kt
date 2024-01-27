846966013e2a4f0502c3060f74a69351778c7cc4
Hlyanhtetkyaw <hhkyaw2911@gmail.com>
2024-01-27 23:16:19 +0630

    add action button in SearchBar

--- app/src/main/java/com/test/newsapp/presentation/SearchBar.kt
+++ app/src/main/java/com/test/newsapp/presentation/SearchBar.kt
@@ -1,6 +1,8 @@
 package com.test.newsapp.presentation
 
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
@@ -14,6 +16,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -35,9 +38,16 @@ fun SearchBar(
         value = text,
         onValueChange = {
             text = it
-            onSearch(text)
+            if (text.isEmpty()) {
+                onSearch(text)
+            }
         },
         label = { Text("Search...") },
+        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
+        keyboardActions = KeyboardActions(
+            onSearch = {
+                onSearch(text)
+            }),
         singleLine = true,
         modifier = modifier,
         shape = RoundedCornerShape(12.dp),
